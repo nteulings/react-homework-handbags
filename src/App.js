292cee8ec6ed669072f4489f22bebf4cdc686b52
Niels Teulings <niels@MacBook-Air-van-Niels.local>
2021-05-23 14:42:43 +0200

    button aangepast app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import './App.css';
 
 function App() {
   return (
+      <>
       <h1>Handbags & Purses</h1>
+      <nav>
+        <button onClick={() => console.log('to the collection') }
+            >to the collection
+        </button>
+        <button onClick={() => console.log('shop all bags') }
+        >shop all bags</button>
+        <button disabled={true}>pre orders</button>
+      </nav>
+
+      </>
   );
 }
 
